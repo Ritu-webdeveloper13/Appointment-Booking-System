f0178a3baaaea8b2cec78d8d1b94fea3f6c94974
Ritu-webdeveloper13 <rituraj13260@gmail.com>
2025-05-05 21:15:15 +0530

    Remove Backend

--- Backend/Routes/adminRouter.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import { addDoctor } from '../Controllers/adminController'
-import upload from '../Middlewares/multer'
-
-const adminRouter = express.Router()
-
-adminRouter.post('/add-doctor',upload.single('image'),addDoctor)
-
-export default adminRouter
